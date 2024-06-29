4369ad970bce2e5e14327db92e0ddbd9a123e66a
FreZYa <ahmetresul159@gmail.com>
2024-06-29 14:17:50 +0300

    Configured docker compose and checks to wait for db

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -5,7 +5,7 @@ import time
 
 from psycopg2 import OperationalError as Psycopg2OpError
 
-from django.db import connections
+# from django.db import connections
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
@@ -17,10 +17,11 @@ class Command(BaseCommand):
         pass
         """Entrypoint for command."""
         self.stdout.write('Waiting for database...')
-        db_conn = None
-        while not db_conn:
+        db_up = False
+        while not db_up:
             try:
-                db_conn = connections['default']
+                self.check(databases=['default'])
+                db_up = True
             except (OperationalError, Psycopg2OpError):
                 self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
